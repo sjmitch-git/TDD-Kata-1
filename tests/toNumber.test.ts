7613c7b597d5ce655d5fa1ff193cb565bb8144fa
sjmitch-git <sjmitch20@outlook.com>
2023-10-30 23:17:44 +0000

    adds tests for toNumber

--- tests/toNumber.test.ts
+++ tests/toNumber.test.ts
@@ -1,7 +1,24 @@
 import { convertToNumber } from "../src";
 
-describe("test convertToNumber function", () => {
-  it("should return number from Roman numeral", () => {
-    expect(convertToNumber("XVII")).toBe(17);
-  });
+describe("convertToNumber", () => {
+  const romanNumerals: { [key: string]: number } = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+  for (const key in romanNumerals) {
+    test(`should convert ${key} to ${romanNumerals[key]}`, () => {
+      expect(convertToNumber(key)).toBe(romanNumerals[key]);
+    });
+  }
 });
